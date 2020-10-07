1523a0d783eb3f6db10ea93b807ae6502a194a47
monicaneill <67064659+monicaneill@users.noreply.github.com>
2020-10-07 19:20:47 +0100

    Update Python_Final_Project.py

--- Python_Final_Project.py
+++ Python_Final_Project.py
@@ -1,3 +1,9 @@
+#Problem Statement: Imagine that you're an IT specialist working in a medium-sized company, your manager wants to create a daily report that tracks the use of machines.
+#Specifically, she wants to know which users are currently connected to which machines, it's your job to create the report. In your company, there's a system that collects
+#every event that happens on the machines on the network. Among the many events collected it records each time a user logs in or out of a computer. With this information,
+#we want to write a script that generates a report of which users are logged in to which machines at that time. We've identified our problem statement which is we need to 
+#process a list of event objects using their date, type, machine, and user attributes to generate a report that lists all users currently logged into the machines.
+
 #Start by defining helper function that we'll use to
 #sort the list
 def get_event_date(event):
